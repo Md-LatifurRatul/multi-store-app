db0cff1f5021fecbf8896a02b281a550954c45ec
LatifurRatul <latifursoikot@gmail.com>
2024-12-13 22:01:59 +0600

    project setup, mvc structure

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:ecom_multi_store/views/buyers/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class MultiStoreApp extends StatelessWidget {
+  const MultiStoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MainScreen(),
+      title: 'Multi Store App',
+    );
+  }
+}
---
+++ lib/views/buyers/main_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Center(child: Text('Multi Store App')),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.yellow.shade900,
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'HOME',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/explore.svg',
+              width: 20,
+            ),
+            label: 'CATEGORIES',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/shop.svg',
+              width: 20,
+            ),
+            label: 'STORE',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/cart.svg',
+              width: 20,
+            ),
+            label: 'CART',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/search.svg',
+              width: 20,
+            ),
+            label: 'SEARCH',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/account.svg',
+              width: 20,
+            ),
+            label: 'ACCOUNT',
+          ),
+        ],
+      ),
+    );
+  }
+}
