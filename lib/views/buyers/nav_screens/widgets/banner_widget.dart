786e98f62b467d306efb973d567e041152751d26
LatifurRatul <latifursoikot@gmail.com>
2025-01-12 23:18:39 +0600

    upload, retrieve banner

--- lib/views/buyers/nav_screens/widgets/banner_widget.dart
+++ lib/views/buyers/nav_screens/widgets/banner_widget.dart
@@ -1,16 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class BannerWidget extends StatelessWidget {
+class BannerWidget extends StatefulWidget {
   const BannerWidget({super.key});
 
   @override
+  State<BannerWidget> createState() => _BannerWidgetState();
+}
+
+class _BannerWidgetState extends State<BannerWidget> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  final List _bannerImage = [];
+
+  getBanners() {
+    return _firestore
+        .collection('banners')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      for (var doc in querySnapshot.docs) {
+        _bannerImage.add(doc['image']);
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getBanners();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.sizeOf(context);
-    return Container(
-      height: 140,
-      width: size.width,
-      decoration: BoxDecoration(
-        color: Colors.yellow.shade900,
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        height: 140,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.yellow.shade900,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: PageView.builder(
+            itemCount: _bannerImage.length,
+            itemBuilder: (context, index) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  _bannerImage[index],
+                  fit: BoxFit.cover,
+                ),
+              );
+            }),
       ),
     );
   }
